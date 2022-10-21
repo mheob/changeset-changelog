193c43e12444af8cb0e9ea51a0c71fc50c6b1856
Alex Böhm <tools@boehm.work>
2022-10-21 10:05:50 +0200

    style: cleanup repository (#11)
    Just cleanup the repository.

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,11 +2,12 @@ module.exports = {
 	...require('@mheob/prettier-config'),
 
 	overrides: [
+		...require('@mheob/prettier-config').overrides,
 		{
 			files: '*.md',
 			options: {
 				printWidth: 999,
-				proseWrap: 'preserve',
+				proseWrap: 'never',
 			},
 		},
 	],
