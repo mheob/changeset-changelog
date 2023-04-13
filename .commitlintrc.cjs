80ad9704b882d10acf2c0efc0f2da8a6bba6a6ef
Alex Böhm <tools@boehm.work>
2023-04-13 16:20:11 +0200

    chore(deps): add own `commitlint-config` and update all dependencies

---
+++ .commitlintrc.cjs
@@ -0,0 +1,10 @@
+const defaultConfig = require('@mheob/commitlint-config');
+
+/** @type {import('cz-git').UserConfig} */
+module.exports = {
+	...defaultConfig,
+	prompt: {
+		...defaultConfig.prompt,
+		allowEmptyScopes: true,
+	},
+};
